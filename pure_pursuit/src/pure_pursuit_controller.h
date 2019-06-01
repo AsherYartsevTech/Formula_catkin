2749a30affea71837f1d8b989dfa518db719afd8
mattibaer02 <48477821+mattibaer02@users.noreply.github.com>
2019-06-01 14:14:00 +0300

    Add files via upload

--- pure_pursuit/src/pure_pursuit_controller.h
+++ pure_pursuit/src/pure_pursuit_controller.h
@@ -11,8 +11,13 @@ using std::pair;
 class PurePursuitController
 {
 private:
-    double ld;
-    double velocity;
+	double ld_time;
+    double max_velocity;
+	double max_accel;
+	double length;
+	double min_ld;
+
+
     vector<pair<double,double>> waypoints;
     //double curr_waypoint;
     int wp_idx;
@@ -51,7 +56,7 @@ private:
 public:
     
     // C'tor
-    PurePursuitController(double ld, double velocity, vector<pair<double,double>> waypoints);
+    PurePursuitController(double ld_time, double max_velocity, double max_accel, double length, double min_ld, vector<pair<double,double>> waypoints);
 
     // D'tor
     ~PurePursuitController();
@@ -61,12 +66,12 @@ public:
     *   car inputs.
     *   Returns the lateral error.
     */
-   double control_step(const vector<double>& Pose, double& velocity,
+    control_step(const vector<double>& Pose, double& velocity,
                        double& alpha)
    {
         double lat_error;
-        // currently the velocity is constant
-        velocity = this->velocity;
+	
+      
 
         pair<double,double> nearest_point;
         pair<double,double> car_pos = std::make_pair(Pose[0], Pose[1]);
@@ -75,37 +80,52 @@ public:
         double err_y = nearest_point.second - car_pos.second;
         lat_error = sqrt(err_x*err_x + err_y*err_y);
 
-        alpha = calc_alpha(Pose);
+        alpha = calc_alpha(Pose, double& velocity);
         if (alpha > M_PI) {
             alpha = -2 * M_PI - alpha;
         } else if (alpha < - M_PI) {
             alpha = 2 * M_PI + alpha;
         }
+
     
         return lat_error;
    }
 
-    double calc_alpha(const vector<double>& Pose)
+    double calc_alpha(const vector<double>& Pose, double& velocity)
     {
+		
         pair<double,double> ld_point;
         pair<double,double> position = std::make_pair(Pose[0], Pose[1]);
-        calc_ld_point(position, ld_point);
+        calc_ld_point(position, ld_point, velocity);
         
         double dx = (ld_point.first - Pose[0]);
         double dy = (ld_point.second - Pose[1]);
-    
+		double ld_dist = sqrt(dx ^ 2 + dy ^ 2);
         double beta = acos((dx)/(sqrt(dx*dx+dy*dy)));
 
         if (dy < 0) {
             beta = 2 * M_PI - beta;
         }
+		beta = beta - Pose[2];
+		double alpha = atan(2 * (this->length)*sin(beta) / ld_dist);
+		if (alpha == 0) {
+			velocity = this->max_velocity;
+		}
+		else {
+			double r = 0.5*(ld_dist / sin(alpha));
+			velocity = sqrt(this->max_accel*r);
+			if (velocity > this->max_velocity) {
+				velocity = this->max_velocity;
+			}
+		}
 
-        return (beta - Pose[2]);
+        return (alpha);
     }
 
     void calc_ld_point(const pair<double,double> car,
-                       pair<double,double>& ld_point)
+                       pair<double,double>& ld_point, velocity)
     {
+		double ld_dist = std::max ((this->ld_time)*velocity , this->min_ld) ;
         int next_wp_idx = (this->wp_idx + 1) % this->waypoints.size();
         pair<double,double> next_wp = this->waypoints[next_wp_idx];
         
@@ -129,12 +149,12 @@ public:
             x0 = last_wp.first;
             a = 1;
             b = (-2) * car.second;
-            c = (pow(car.first-x0, 2) + pow(car.second,2) - pow(this->ld,2));
+            c = (pow(car.first-x0, 2) + pow(car.second,2) - pow(ld_dist,2));
         } else {
             y0 = last_wp.second - (m * last_wp.first);
             a = 1 + pow(m,2);
             b = (-2) * car.first + 2 * m * (y0-car.second);
-            c = (pow(car.first,2) + pow(y0-car.second,2) - pow(this->ld,2));
+            c = (pow(car.first,2) + pow(y0-car.second,2) - pow(ld_dist,2));
         }
         
         pair<double,double> roots = get_polynomial_roots(a,b,c);
@@ -186,7 +206,7 @@ public:
 
 };
 
-PurePursuitController::PurePursuitController(double ld, double velocity,
+PurePursuitController::PurePursuitController(double ld_time , double velocity,
  vector<pair<double,double>> waypoints) : ld(ld), velocity(velocity), 
  waypoints(waypoints), wp_idx(0)
 {
